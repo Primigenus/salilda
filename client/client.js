0fb4be80b4b24bd7faa6e9f1b6dc234e835fc08c
Rahul Choudhury <rchoudhury@gmail.com>
2015-05-28 17:38:34 -0700

    Declare curSongs before assigning it Reuse curPage instead of using Songs.find().count() multiple times

--- client/client.js
+++ client/client.js
@@ -58,17 +58,18 @@ Template.songs.events({
 Template.songs.helpers({
   nextPage: function() {
     var totalSongs = SongCount.findOne().count;
+    var curSongs;
 
     var curPage = Template.instance().currentPage.get();
     if (curPage > 1) {
-      var prevSongs = 50 * (Template.instance().currentPage.get() - 1);
-      var curSongs = prevSongs + Songs.find().count();
+      var prevSongs = 50 * (curPage - 1);
+      curSongs = prevSongs + Songs.find().count();
     } else {
-      var curSongs = 50;
+      curSongs = 50;
     }
-    
+
     if (curSongs < totalSongs)
-      return Template.instance().currentPage.get() + 1;
+      return curPage + 1;
   },
   prevPage: function() {
     var curPage = Template.instance().currentPage.get();
